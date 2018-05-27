8bf98fe442a941c83912278a564a4aa00cba9320
Guillaume Limberger <glim.dev@gmail.com>
2018-05-27 09:58:44 +0200

    [Client] Refactoring in componnents

--- client/src/components/SampleViewLayout.js
+++ client/src/components/SampleViewLayout.js
@@ -3,12 +3,16 @@ import { Container, Header } from 'semantic-ui-react'
 
 import SampleViewList from './SampleViewList'
 
-const SampleViewLayout = props => (
-  <Container style={{marginTop: '3em'}}>
-    <Header as='h2'>Sample List</Header>
+const SampleViewLayout = props => {
+  const {samples} = props
 
-    <SampleViewList samples={props.samples}/>
-  </Container>
-)
+  return (
+    <Container style={{marginTop: '3em'}}>
+      <Header as='h2'>Sample List</Header>
+
+      <SampleViewList samples={samples}/>
+    </Container>
+  )
+}
 
 export default SampleViewLayout
--- client/src/components/SampleViewList.js
+++ client/src/components/SampleViewList.js
@@ -3,25 +3,30 @@ import React from 'react'
 import { List } from 'semantic-ui-react'
 
 const handleItemClick = sample => {
-  console.log('handleItemClick', sample.filename)
   let audio = new Audio('http://localhost:3000/static/audio/' + sample.filename)
-  console.log('audio', audio)
-  audio.play().then(() => console.log('audio played'))
+
+  audio
+    .play()
+    .then(() => console.log('audio played'))
 }
 
-const SampleViewList = props => (
-  <List celled>
-    {props.samples.map(s => <List.Item key={s.id}>
-      <List.Icon size='big' color='violet' name='file audio outline' onClick={() => handleItemClick(s)} link/>
-      <List.Content>
-        <List.Header><h4>{s.filename}</h4> ({s.path})</List.Header>
-        <List.Description>
-          Created: {s.createdAt} — Updated: {s.updatedAt} <br/>
-          Label: {s.label || '~'} Group: {s.group || '~'}
-        </List.Description>
-      </List.Content>
-    </List.Item>)}
-  </List>
-)
+const SampleViewList = props => {
+  const { samples } = props
+
+  return (
+    <List celled>
+      {samples.map(s => <List.Item key={s.id}>
+        <List.Icon size='big' color='violet' name='file audio outline' onClick={() => handleItemClick(s)} link/>
+        <List.Content>
+          <List.Header><h4>{s.filename}</h4> ({s.path})</List.Header>
+          <List.Description>
+            Created: {s.createdAt} — Updated: {s.updatedAt} <br/>
+            Label: {s.label || '~'} Group: {s.group || '~'}
+          </List.Description>
+        </List.Content>
+      </List.Item>)}
+    </List>
+  )
+}
 
 export default SampleViewList
