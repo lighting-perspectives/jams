b061b670b8f838fea22dc8eaf5116872a160baf0
Guillaume Limberger <glim.dev@gmail.com>
2018-05-30 22:15:07 +0200

    [Client] Minor refactoring

--- client/src/components/admin/FormSampleNew.js
+++ client/src/components/admin/FormSampleNew.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react'
 import { Button, Form, Icon } from 'semantic-ui-react'
+import config from '../../config/config'
 
 export default class FormSampleNew extends Component {
   constructor (props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleFileInputChange = this.handleFileInputChange.bind(this)
     this.updateSampleList = props.updateSampleList
     this.state = {
-      sampleCreating: false,
-      file: null,
-      label: null,
-      group: null
+      isCreatingSample: false
     }
   }
 
-  handleSubmit (e) {
-    this.setState({sampleCreating: true})
-    e.preventDefault()
+  handleSubmit (event) {
+    event.preventDefault()
 
-    let form = e.target
-    const formData = new FormData(form)
+    this.setState({isCreatingSample: true})
 
-    fetch('http://localhost:3000/api/samples', {
+    let form = event.target
+
+    fetch(config.SERVER.BASE_URL + config.SERVER.SAMPLE_RESOURCE, {
       method: 'POST',
-      body: formData
+      body: new FormData(form)
     })
       .then(res => res.json())
       .then(sample => {
-        console.log(sample)
+        console.log('new Sample', sample)
         this.updateSampleList()
         this.resetFileInput(form)
-        this.setState({sampleCreating: false})
+        this.setState({isCreatingSample: false})
       })
       .catch(err => {
         console.error('Fetch Error :', err)
       })
-  }
-
-  handleFileInputChange (e) {
-    const files = e.target.files
-    console.log('files', e.target.files)
-
-    this.setState({file: files[0]})
   }
 
   resetFileInput (form) {
@@ -53,11 +43,11 @@ export default class FormSampleNew extends Component {
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='Audio file' type='file' name='audioFile' onChange={this.handleFileInputChange} />
+          <Form.Input fluid label='Audio file' type='file' name='audioFile' />
           <Form.Input fluid label='Label' name='label' />
           <Form.Input fluid label='Group' name='group' />
         </Form.Group>
-        <Button icon type='submit' loading={this.state.sampleCreating}>
+        <Button icon type='submit' loading={this.state.isCreatingSample}>
           <Icon name='file audio outline' />
           &nbsp;New Sample
         </Button>
