0fa992565e1ec196ed629ba4106c6b3069086ae2
Guillaume Limberger <glim.dev@gmail.com>
2018-06-21 22:02:05 +0200

    [Server] Fix controllers index.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,7 @@
-const todos = require('./todos')
-const todoItems = require('./todoitems')
-const samples = require('./samples')
 const instruments = require('./instruments')
 const instrumentMappings = require('./instrument-mappings')
 
 module.exports = {
-  todos,
-  todoItems,
-  samples,
   instruments,
   instrumentMappings
 }
