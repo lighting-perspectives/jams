d20cd9132835e1f1f66ac390bc3250e068bd52a6
Guillaume Limberger <glim.dev@gmail.com>
2018-06-21 21:33:36 +0200

    [Server] Add send middleware.

---
+++ server/middlewares/sample/send.js
@@ -0,0 +1,23 @@
+/**
+ * Sends the response.
+ *
+ * res.extra.sample or res.extra.samples is required and is normally set in sampleCreate, sampleUpdate,
+ * sampleFindById, sampleFindAll middlewares.
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+const send = (req, res, next) => {
+  if (res.statusCode === 204) { // No content
+    res.end()
+  } else if (res.extra && res.extra.sample) {
+    res.send(res.extra.sample)
+  } else if (res.extra && res.extra.samples) {
+    res.send(res.extra.samples)
+  } else {
+    next(new Error('The \'sample(s)\' property of response extra is missing.'))
+  }
+}
+
+module.exports = send
