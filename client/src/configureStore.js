266817d07d610f0fe535e5dbd1ce86324eab7c38
Guillaume Limberger <glim.dev@gmail.com>
2018-06-22 20:18:23 +0200

    [Client] Add redux devtools extension

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+
 import rootReducer from './reducers'
 
 export default function configureStore () {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
   )
 }
