d941b43479b8e762e70fac90c2f65207a186408b
Guillaume Limberger <glim.dev@gmail.com>
2018-07-06 08:03:06 +0200

    [Server] Set error handler middleware as named function.
    Set display as json.

--- server/app.js
+++ server/app.js
@@ -58,13 +58,12 @@ app.use(function handleMulterError (err, req, res, next) {
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function handleGenericError (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500).render('error')
+  res.status(err.status || 500).json(err)
 })
 
 module.exports = app
